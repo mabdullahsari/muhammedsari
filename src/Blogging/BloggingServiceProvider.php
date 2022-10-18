0c032d45876b934f523c0478591087e7b695b7a5
Muhammed Sari <mabdullahsari@hotmail.com>
2022-10-18 22:20:17 +0200

    Single SP per context

--- src/Blogging/BloggingServiceProvider.php
+++ src/Blogging/BloggingServiceProvider.php
@@ -2,21 +2,37 @@
 
 namespace Domain\Blogging;
 
+use Illuminate\Contracts\Auth\Access\Gate;
 use Illuminate\Database\Eloquent\Relations\Relation;
-use Illuminate\Support\AggregateServiceProvider;
+use Illuminate\Support\ServiceProvider;
 
-final class BloggingServiceProvider extends AggregateServiceProvider
+final class BloggingServiceProvider extends ServiceProvider
 {
-    protected $providers = [
-        AuthServiceProvider::class,
+    private array $morphMap = [
+        'author' => Author::class,
+        'post' => Post::class,
+        'tag' => Tag::class,
+    ];
+
+    private array $policyMap = [
+        Post::class => PostPolicy::class,
+        Tag::class => TagPolicy::class,
     ];
 
     public function boot(): void
     {
-        Relation::enforceMorphMap([
-            'author' => Author::class,
-            'post' => Post::class,
-            'tag' => Tag::class,
-        ]);
+        Relation::enforceMorphMap($this->morphMap);
+    }
+
+    public function register(): void
+    {
+        $this->app->resolving(Gate::class, $this->registerPolicies(...));
+    }
+
+    private function registerPolicies(Gate $access): void
+    {
+        foreach ($this->policyMap as $model => $policy) {
+            $access->policy($model, $policy);
+        }
     }
 }
--- src/Identity/IdentityServiceProvider.php
+++ src/Identity/IdentityServiceProvider.php
@@ -2,17 +2,30 @@
 
 namespace Domain\Identity;
 
+use Illuminate\Contracts\Auth\Access\Gate;
 use Illuminate\Database\Eloquent\Relations\Relation;
-use Illuminate\Support\AggregateServiceProvider;
+use Illuminate\Support\ServiceProvider;
 
-final class IdentityServiceProvider extends AggregateServiceProvider
+final class IdentityServiceProvider extends ServiceProvider
 {
-    protected $providers = [
-        AuthServiceProvider::class,
+    private array $policyMap = [
+        User::class => UserPolicy::class,
     ];
 
     public function boot(): void
     {
         Relation::enforceMorphMap(['user' => User::class]);
+    }
+
+    public function register(): void
+    {
+        $this->app->resolving(Gate::class, $this->registerPolicies(...));
+    }
+
+    private function registerPolicies(Gate $access): void
+    {
+        foreach ($this->policyMap as $model => $policy) {
+            $access->policy($model, $policy);
+        }
     }
 }
