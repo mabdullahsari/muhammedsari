63f3cc68a9a014f39a5f358e2baae9287110b1a2
Muhammed Sari <mabdullahsari@hotmail.com>
2022-10-22 01:39:03 +0200

    Use DateTimeZone for validation

--- src/Clock/NativeClock.php
+++ src/Clock/NativeClock.php
@@ -3,13 +3,17 @@
 namespace Domain\Clock;
 
 use Carbon\CarbonImmutable;
+use DateTimeZone;
 use Domain\Clock\Contracts\Clock;
 
 final class NativeClock implements Clock
 {
-    public function __construct(
-        private readonly string $timezone,
-    ) {}
+    private readonly DateTimeZone $timezone;
+
+    public function __construct(string $timezone)
+    {
+        $this->timezone = new DateTimeZone($timezone);
+    }
 
     public function now(): CarbonImmutable
     {
