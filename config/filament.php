de75ade58ba28211e4cd40949588bd75f47c50a1
Muhammed Sari <mabdullahsari@hotmail.com>
2022-10-22 00:28:12 +0200

    Customize filament

--- config/filament.php
+++ config/filament.php
@@ -140,7 +140,6 @@ return [
         'path' => app_path('Filament/Widgets'),
         'register' => [
             Widgets\AccountWidget::class,
-            Widgets\FilamentInfoWidget::class,
         ],
     ],
 
@@ -236,12 +235,12 @@ return [
             'have_inline_labels' => false,
         ],
         'footer' => [
-            'should_show_logo' => true,
+            'should_show_logo' => false,
         ],
         'max_content_width' => null,
         'notifications' => [
             'vertical_alignment' => 'top',
-            'alignment' => 'right',
+            'alignment' => 'center',
         ],
         'sidebar' => [
             'is_collapsible_on_desktop' => false,
