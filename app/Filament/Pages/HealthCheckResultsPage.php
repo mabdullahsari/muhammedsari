197e9cd98f1571c7d536e47936487850b729f35d
Muhammed Sarı <24608797+mabdullahsari@users.noreply.github.com>
2022-10-24 23:51:07 +0200

    Change page slug

--- app/Filament/Pages/HealthCheckResultsPage.php
+++ app/Filament/Pages/HealthCheckResultsPage.php
@@ -4,7 +4,9 @@ namespace App\Filament\Pages;
 
 use ShuvroRoy\FilamentSpatieLaravelHealth\Pages\HealthCheckResults;
 
-class HealthCheckResultsPage extends HealthCheckResults
+final class HealthCheckResultsPage extends HealthCheckResults
 {
     protected static ?int $navigationSort = 5;
+
+    protected static ?string $slug = 'application-health';
 }
