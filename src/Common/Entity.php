461a6497cfd8ec3232431631b4fc980e8f3c80f4
Muhammed Sari <mabdullahsari@hotmail.com>
2022-10-31 16:18:46 +0100

    Merge concerns

--- src/Common/Entity.php
+++ src/Common/Entity.php
@@ -4,6 +4,33 @@ namespace Domain\Common;
 
 abstract class Entity
 {
-    use HasEvents;
-    use Persistable;
+    private array $events = [];
+
+    private bool $wasRecentlyCreated = true;
+
+    public function flushEvents(): array
+    {
+        $events = $this->events;
+
+        $this->events = [];
+
+        return $events;
+    }
+
+    public function markAsPersisted(): static
+    {
+        $this->wasRecentlyCreated = false;
+
+        return $this;
+    }
+
+    public function wasRecentlyCreated(): bool
+    {
+        return $this->wasRecentlyCreated;
+    }
+
+    protected function raise(object $event): void
+    {
+        $this->events[] = $event;
+    }
 }
