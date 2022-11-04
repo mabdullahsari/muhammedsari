85d9018ddbf24bdd5cd2d52dced8b852d9784ae4
Muhammed Sari <mabdullahsari@hotmail.com>
2022-11-04 23:32:23 +0100

    Remove useless middleware

--- app/Foundation/Kernel.php
+++ app/Foundation/Kernel.php
@@ -7,9 +7,7 @@ use Illuminate\Foundation\Http\Kernel as HttpKernel;
 final class Kernel extends HttpKernel
 {
     protected $middleware = [
-        \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \Illuminate\Foundation\Http\Middleware\TrimStrings::class,
-        \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
     ];
 
     protected $middlewareGroups = [
@@ -17,13 +15,7 @@ final class Kernel extends HttpKernel
             \Illuminate\Cookie\Middleware\EncryptCookies::class,
             \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
             \Illuminate\Session\Middleware\StartSession::class,
-            \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \Illuminate\Foundation\Http\Middleware\VerifyCsrfToken::class,
-            \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
-    ];
-
-    protected $routeMiddleware = [
-        'auth' => \Illuminate\Auth\Middleware\Authenticate::class,
     ];
 }
