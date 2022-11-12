cb8c7e0a5505ccf3cee8501f41c6195cc20a7b62
Muhammed Sari <mabdullahsari@hotmail.com>
2022-11-12 21:27:17 +0100

    Add HttpKernel back

--- app/Foundation/RouteServiceProvider.php
+++ app/Foundation/RouteServiceProvider.php
@@ -2,23 +2,12 @@
 
 namespace App\Foundation;
 
-use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
 
 final class RouteServiceProvider extends ServiceProvider
 {
-    private array $web = [
-        \Illuminate\Cookie\Middleware\EncryptCookies::class,
-        \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
-        \Illuminate\Session\Middleware\StartSession::class,
-        \Illuminate\Foundation\Http\Middleware\VerifyCsrfToken::class,
-    ];
-
     public function boot(): void
     {
-        $this->app->make(Kernel::class)->pushMiddleware(StripTrailingSlash::class);
-
-        $this->middlewareGroup('web', $this->web);
         $this->loadRoutesFrom(__DIR__ . '/../../routes/web.php');
     }
 }
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -28,7 +28,7 @@ $app = new Illuminate\Foundation\Application(
 
 $app->singleton(
     Illuminate\Contracts\Http\Kernel::class,
-    Illuminate\Foundation\Http\Kernel::class,
+    App\Foundation\Kernel::class,
 );
 
 $app->singleton(
