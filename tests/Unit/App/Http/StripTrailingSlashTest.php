301efff4b4074aa940a01112e1887e5d42f99247
Muhammed Sari <mabdullahsari@hotmail.com>
2022-11-15 09:33:14 +0100

    Move test

--- tests/Unit/App/Http/StripTrailingSlashTest.php
+++ tests/Unit/App/Http/StripTrailingSlashTest.php
@@ -1,6 +1,6 @@
-<?php
+<?php declare(strict_types=1);
 
-namespace Tests\Unit\App\Foundation;
+namespace Tests\Unit\App\Http;
 
 use App\Http\StripTrailingSlash;
 use Illuminate\Http\RedirectResponse;
