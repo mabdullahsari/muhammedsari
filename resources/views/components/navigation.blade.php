6105ebf2647285d637d343c42eb26cee4e34c7d8
Muhammed Sari <mabdullahsari@hotmail.com>
2023-05-06 17:35:09 +0200

    Add logo

--- resources/views/components/navigation.blade.php
+++ resources/views/components/navigation.blade.php
@@ -1,14 +1,16 @@
 <header class="fixed top-0 left-0 w-full border-b border-zinc-800 bg-zinc-900/90 backdrop-blur-[6px]">
-    <nav class="flex items-center mx-auto max-w-6xl px-6 py-8">
+    <nav class="flex items-center mx-auto max-w-5xl px-6 py-4">
         <h2 class="sr-only">Main navigation</h2>
 
-        <i class="flex-1">
-            <a href="{{ $home }}" class="block outline-none hover:text-zinc-200 focus:text-zinc-200">{{ $name }}</a>
+        <i class="mr-auto">
+            <a href="{{ $home }}" title="{{ $name }}" class="block outline-none text-white transition-colors hover:text-amber-600">
+                <x-icon name="ms" class="h-12 -my-2 sm:m-0 sm:h-16" />
+            </a>
         </i>
 
         <i class="pl-5 relative sm:hidden">
             <button id="mobile-nav-open" aria-label="menu" class="block cursor-pointer p-2 bg-zinc-800 rounded-lg">
-                <x-icon name="o-bars-3" />
+                <x-icon name="o-bars-3" class="w-5 h-5" />
             </button>
         </i>
 
@@ -31,7 +33,7 @@
     <i class="fixed inset-x-4 top-8 z-50 rounded-3xl p-8 ring-1 bg-zinc-900 ring-zinc-800" tabindex="-1">
         <i class="flex flex-row-reverse justify-between">
             <button id="mobile-nav-close" aria-label="close" class="-m-1 p-1" type="button" tabindex="0">
-                <x-icon name="o-x-mark" class="text-zinc-400" />
+                <x-icon name="o-x-mark" class="text-zinc-400 w-5 h-5" />
             </button>
 
             <span class="text-sm font-medium text-zinc-400">Navigation</span>
