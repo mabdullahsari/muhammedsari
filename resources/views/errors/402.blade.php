5d450ade7d49f4178c025fe693e1dce0e8aa335e
Muhammed Sari <mabdullahsari@hotmail.com>
2023-05-10 00:43:11 +0200

    Get rid of custom error views

--- resources/views/errors/402.blade.php
+++
@@ -1,5 +0,0 @@
-<x-page name="402" class="max-w-3xl">
-    <x-heading class="text-center mb-5 sm:mb-10">402</x-heading>
-
-    <x-intro class="text-center mx-auto">Payment Required</x-intro>
-</x-page>
