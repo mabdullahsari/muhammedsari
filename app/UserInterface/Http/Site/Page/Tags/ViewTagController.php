b25c00306f24b4a45daa9c82584434e72cd8d9b4
Muhammed Sari <mabdullahsari@hotmail.com>
2023-05-09 21:04:36 +0200

    Content negotiation

--- app/UserInterface/Http/Site/Page/Tags/ViewTagController.php
+++ app/UserInterface/Http/Site/Page/Tags/ViewTagController.php
@@ -4,22 +4,29 @@ namespace App\UserInterface\Http\Site\Page\Tags;
 
 use Blogging\Contract\GetMyPostsByTag;
 use Blogging\Contract\GetSingleTag;
-use Illuminate\Contracts\View\Factory;
+use Blogging\CouldNotFindTag;
+use Illuminate\Contracts\Routing\ResponseFactory;
 use Illuminate\Contracts\View\View;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 final readonly class ViewTagController
 {
     public function __construct(
         private GetMyPostsByTag $posts,
+        private ResponseFactory $response,
         private GetSingleTag $tags,
-        private Factory $view,
     ) {}
 
-    public function __invoke(string $slug): View
+    /** @throws CouldNotFindTag */
+    public function __invoke(Request $request, string $slug): Response
     {
-        return $this->view->make('view-tag', [
-            'tag' => ($tag = $this->tags->findBySlug($slug)),
-            'posts' => $this->posts->getByTag($tag),
-        ]);
+        $posts = $this->posts->getByTag($tag = $this->tags->findBySlug($slug));
+
+        if ($request->expectsJson()) {
+            return $this->response->json($posts);
+        }
+
+        return $this->response->view('view-tag', ['posts' => $posts, 'tag' => $tag]);
     }
 }
--- app/UserInterface/Http/Site/Page/Tags/ViewTagsController.php
+++ app/UserInterface/Http/Site/Page/Tags/ViewTagsController.php
@@ -3,17 +3,29 @@
 namespace App\UserInterface\Http\Site\Page\Tags;
 
 use Blogging\Contract\GetAllTags;
+use Illuminate\Contracts\Routing\ResponseFactory;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 final readonly class ViewTagsController
 {
     public const ROUTE = 'tags';
 
-    public function __construct(private GetAllTags $tags, private Factory $view) {}
+    public function __construct(
+        private ResponseFactory $response,
+        private GetAllTags $tags,
+    ) {}
 
-    public function __invoke(): View
+    public function __invoke(Request $request): Response
     {
-        return $this->view->make('view-tags', ['tags' => $this->tags->get()]);
+        $tags = $this->tags->get();
+
+        if ($request->expectsJson()) {
+            return $this->response->json($tags);
+        }
+
+        return $this->response->view('view-tags', ['tags' => $tags]);
     }
 }
