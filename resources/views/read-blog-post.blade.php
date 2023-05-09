4c01f72cbe08c45418a4395d631d2de212cb6556
Muhammed Sari <mabdullahsari@hotmail.com>
2023-05-09 21:33:22 +0200

    Prevent crashes when post is unpublished

--- resources/views/read-blog-post.blade.php
+++ resources/views/read-blog-post.blade.php
@@ -1,7 +1,7 @@
 <x-page :description="$summary" :name="$title" :og="route('og.post', $slug)" class="max-w-3xl">
     <i class="flex flex-col mb-8 sm:flex-row sm:items-center">
         <i class="flex-1 pr-5 text-sm">
-            {{ $publishedAt->format('F jS, Y') }}
+            {{ $publishedAt ? $publishedAt->format('F jS, Y') : 'Top secret' }}
         </i>
 
         <i class="mt-5 flex-wrap inline-flex items-center">
