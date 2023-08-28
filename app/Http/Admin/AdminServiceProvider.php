998f8e71f0bd262d36eba96c1fbcd75c687cd6c5
Muhammed Sari <mabdullahsari@hotmail.com>
2023-08-28 05:19:58 +0200

    Do not auto-discover new dependencies

--- app/Http/Admin/AdminServiceProvider.php
+++ app/Http/Admin/AdminServiceProvider.php
@@ -2,7 +2,11 @@
 
 namespace App\Http\Admin;
 
+use Filament\Actions\ActionsServiceProvider;
+use Filament\Infolists\InfolistsServiceProvider;
+use Filament\Widgets\WidgetsServiceProvider;
 use Illuminate\Support\AggregateServiceProvider;
+use Kirschbaum\PowerJoins\PowerJoinsServiceProvider;
 
 final class AdminServiceProvider extends AggregateServiceProvider
 {
@@ -20,11 +24,15 @@ final class AdminServiceProvider extends AggregateServiceProvider
         \App\Http\RouteServiceProvider::class,
         \BladeUI\Icons\BladeIconsServiceProvider::class,
         \BladeUI\Heroicons\BladeHeroiconsServiceProvider::class,
+        \Kirschbaum\PowerJoins\PowerJoinsServiceProvider::class,
         \Filament\FilamentServiceProvider::class,
+        \Filament\Actions\ActionsServiceProvider::class,
         \Filament\Forms\FormsServiceProvider::class,
+        \Filament\Infolists\InfolistsServiceProvider::class,
         \Filament\Notifications\NotificationsServiceProvider::class,
         \Filament\Support\SupportServiceProvider::class,
         \Filament\Tables\TablesServiceProvider::class,
+        \Filament\Widgets\WidgetsServiceProvider::class,
         \Livewire\LivewireServiceProvider::class,
         \RyanChandler\BladeCaptureDirective\BladeCaptureDirectiveServiceProvider::class,
         \ShuvroRoy\FilamentSpatieLaravelHealth\FilamentSpatieLaravelHealthServiceProvider::class,
